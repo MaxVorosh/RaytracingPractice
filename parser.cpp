0e90a43624f798db7b3e7d0a4e16e4137d3693b1
MaxVorosh <ma_voroshilov@mail.ru>
2025-03-05 04:49:14 -0500

    Add light structure and update parser

--- parser.cpp
+++ parser.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <fstream>
 #include <sstream>
+#include <iostream>
 
 Scene parse(std::string filename) {
     std::ifstream fin(filename);
@@ -79,6 +80,52 @@ Scene parse(std::string filename) {
             float r, g, b;
             sin >> r >> g >> b;
             scene.objects.back().color = glm::vec3(r, g, b);
+        }
+        else if (command == "METALLIC") {
+            scene.objects.back().material = Material::Metallic;
+        }
+        else if (command == "DIELECTRIC") {
+            scene.objects.back().material = Material::Dielectric;
+        }
+        else if (command == "IOR") {
+            float ior;
+            sin >> ior;
+            scene.objects.back().ior = ior;
+        }
+        else if (command == "RAY_DEPTH") {
+            sin >> scene.recursion_depth;
+        }
+        else if (command == "AMBIENT_LIGHT") {
+            float r, g, b;
+            sin >> r >> g >> b;
+            scene.ambient_light = glm::vec3(r, g, b);
+        }
+        else if (command == "NEW_LIGHT") {
+            scene.lights.push_back(Light());
+        }
+        else if (command == "LIGHT_INTENSITY") {
+            float r, g, b;
+            sin >> r >> g >> b;
+            scene.lights.back().intensity = glm::vec3(r, g, b);
+        }
+        else if (command == "LIGHT_DIRECTION") {
+            float x, y, z;
+            sin >> x >> y >> z;
+            scene.lights.back().config = DirectLightConfig(glm::vec3(x, y, z));
+        }
+        else if (command == "LIGHT_POSITION") {
+            float x, y, z;
+            sin >> x >> y >> z;
+            PointLightConfig conf = std::get<PointLightConfig>(scene.lights.back().config);
+            conf.position = glm::vec3(x, y, z);
+            scene.lights.back().config = conf;
+        }
+        else if (command == "LIGHT_ATTENUATION") {
+            float c0, c1, c2;
+            sin >> c0 >> c1 >> c2;
+            PointLightConfig conf = std::get<PointLightConfig>(scene.lights.back().config);
+            conf.attenuation = glm::vec3(c0, c1, c2);
+            scene.lights.back().config = conf;
         }
     }
     return scene;
