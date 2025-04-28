4e4be371acc276ecb88d2dc269063ed6d95266ac
MaxVorosh <ma_voroshilov@mail.ru>
2025-04-28 14:52:22 -0400

    Add multithreading

--- main.cpp
+++ main.cpp
@@ -1,32 +1,34 @@
 #include <iostream>
 #include <string>
+#include <omp.h>
 #include "parser.h"
 #include "scene.h"
 #include "image_writer.h"
 
 void fill_scene(Scene& scene, ScenePixels& result_scene) {
-    for (int i = 0; i < result_scene.width; ++i) {
-        for (int j = 0; j < result_scene.height; ++j) {
-            glm::vec3 result_color = glm::vec3(0.0);
-            for (int k = 0; k < scene.samples; ++k) {
-                Ray r = generate_ray(scene, i, j);
-                auto inter = intersection(r, scene, 0);
-                glm::vec3 col = inter.second;
-                if (std::isnan(col.x)) {
-                    col.x = 0;
-                }
-                if (std::isnan(col.y)) {
-                    col.y = 0;
-                }
-                if (std::isnan(col.z)) {
-                    col.z = 0;
-                }
-                result_color += col;
+    #pragma omp parallel for
+    for (int ui = 0; ui < result_scene.width * result_scene.height; ++ui) {
+        int i = ui / result_scene.height;
+        int j = ui % result_scene.height;
+        glm::vec3 result_color = glm::vec3(0.0);
+        for (int k = 0; k < scene.samples; ++k) {
+            Ray r = generate_ray(scene, i, j);
+            auto inter = intersection(r, scene, 0);
+            glm::vec3 col = inter.second;
+            if (std::isnan(col.x)) {
+                col.x = 0;
             }
-            result_color /= float(scene.samples);
-            Color converted_color = Color(convert_color(result_color.x), convert_color(result_color.y), convert_color(result_color.z));
-            result_scene.pixels[i + j * result_scene.width] = converted_color;
+            if (std::isnan(col.y)) {
+                col.y = 0;
+            }
+            if (std::isnan(col.z)) {
+                col.z = 0;
+            }
+            result_color += col;
         }
+        result_color /= float(scene.samples);
+        Color converted_color = Color(convert_color(result_color.x), convert_color(result_color.y), convert_color(result_color.z));
+        result_scene.pixels[i + j * result_scene.width] = converted_color;
     }
 }
 
